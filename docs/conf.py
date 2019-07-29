1d8553aaf924f28e56d2cdcb4fbc2e4e49842ed3
Alexander Ioannidis <a.ioannidis@cern.ch>
2019-07-29 18:34:01 +0200

    docs: fix intershpinx mappings

--- docs/conf.py
+++ docs/conf.py
@@ -316,9 +316,12 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/': None,
-    'flask': ('http://flask.pocoo.org/docs/0.11', None),
-    'invenio-pidstore': ('http://pythonhosted.org/invenio-pidstore', None),
-    'invenio-records': ('http://pythonhosted.org/invenio-records', None),
+    'flask': (
+        'http://flask.pocoo.org/docs/', None),
+    'invenio-pidstore': (
+        'https://invenio-pidstore.readthedocs.io/en/latest/', None),
+    'invenio-records': (
+        'https://invenio-records.readthedocs.io/en/latest/', None),
 }
 
 # Autodoc configuraton.
